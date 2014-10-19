b7f7ff7a014dc1f519793ab386a17855f638627a
aiolos404 <aiolos404@gmail.com>
2014-10-19 10:47:31 -0400

    Lecture 5 3/28

---
+++ src/music/Music.java
@@ -0,0 +1,17 @@
+package music;
+
+class Music {
+	private static double _pitch = 440;
+	public static void play(int duration){
+			System.out.println("play for " + duration/1000.0 + ": " + _pitch);
+	}
+	public static void rest(int duration){
+		System.out.println("rest for " + duration/1000.0);
+	}
+	public static void scalePitch(double factor){
+		_pitch *= factor;
+	}
+	public static void reset(){
+		_pitch = 440;
+	}
+}
