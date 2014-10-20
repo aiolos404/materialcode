b87feca766e66b232ddf0068320ee90dcefdaa41
aiolos404 <aiolos404@gmail.com>
2014-10-20 09:30:08 -0400

    Lecture 2 11/46
    Fail to compile

---
+++ src/types/visibility/y/C.java
@@ -0,0 +1,14 @@
+//Fail to compile
+package types.visibility.y;
+import types.visibility.x.A;
+class C {
+	int fy(C that){
+		return this.x0+this.x1+this.x2+this.x3+that.x0+that.x1+that.x2+that.x3;
+	}
+}
+
+class D{
+	int gy(A that){
+		return that.x0 + that.x1 + that.x2 + that.x3;
+	}
+}
