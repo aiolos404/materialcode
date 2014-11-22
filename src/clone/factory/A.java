854400078814e099385e9c115891854728eba3bf
aiolos404 <aiolos404@gmail.com>
2014-11-22 11:44:45 -0600

    Lecture 8 5/34

---
+++ src/clone/factory/A.java
@@ -0,0 +1,5 @@
+package clone.factory;
+
+public class A {
+	protected A(){}
+}
---
+++ src/clone/factory/main/Main.java
@@ -0,0 +1,16 @@
+package clone.factory.main;
+import clone.factory.*;
+
+class B extends A implements Cloneable{
+	public Object clone() throws CloneNotSupportedException{
+		return super.clone();
+	}
+}
+
+public class Main {
+	public static void main(String[] args) throws CloneNotSupportedException{
+		B x = new B();
+		A y = (A) x.clone();
+		B z = (B) x.clone();
+	}
+}
